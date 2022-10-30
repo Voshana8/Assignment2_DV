3c41a6d7b467b96156651627e6e92c66e66792a9
Voshana8 <114582552+Voshana8@users.noreply.github.com>
2022-10-30 23:30:21 +1100

    Uploaded files for Assignment 2

---
+++ maps.js
@@ -0,0 +1,25 @@
+var vg_1 = "life_lader_chloropeth.vg.json";
+vegaEmbed("#life_ladder", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_2 = "proportional_symbol_map.vg.json";
+vegaEmbed("#prop_map", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3= "poor_country.vg.json";
+vegaEmbed("#poorcont", vg_3).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4= "richest_country_line.json";
+vegaEmbed("#richcont", vg_4).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5= "life_expectancy.vg.json";
+vegaEmbed("#lifeexpectancy", vg_5).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
